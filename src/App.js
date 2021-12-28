5480dc364e94c2e5362aa706654a0f41eb36c709
Harun Kisac <h.kisac@hymnai.com>
2021-12-28 00:59:02 +0000

    create react app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./kypnt.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>We develop great ideas with passion.</p>
+        <a
+          className="App-link"
+          href="https://kypnt.co.uk/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Keypoint Solutions
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
